a70929acb544d41ecea2fb0ba6d2081332af3993
jerremeirago <jerremeir@gmail.com>
2025-09-05 18:44:32 +0800

    fix scoreboard data

--- app/Repositories/Nfl/NflApiRepository.php
+++ app/Repositories/Nfl/NflApiRepository.php
@@ -52,9 +52,60 @@ class NflApiRepository
         return collect($response['scores']['category']['match']);
     }
 
+    public function getFullSchedule() 
+    {
+       $data = NflApiResponse::getFirstByField('date_fetched', date('Y-m-d'));
+
+       if (empty($data)) {
+        $data = NflApiResponse::getFirstByField('date_fetched', date('Y-m-d', strtotime('-1 day')));
+       }
+
+       $data = json_decode($data->response, true);
+
+       return $data;
+    }
+
+    public function findTheCurrentWeek()
+    {
+        $data = $this->getFullSchedule();
+
+        $data = collect($data['shedules']['tournament'])->map(function($item){
+            foreach ($item['week'] as $week){
+                
+                foreach($week['matches'] as $match){
+                    
+                    foreach ($match['match'] as $b){
+                        $dateTimeUtc = Carbon::parse($b['datetime_utc'], 'UTC')->setTimeZone('Australia/Sydney')->format('Y-m-d');
+
+                        $dateToday = Carbon::parse(date('Y-m-d'));
+
+                        if ($dateToday->lte($dateTimeUtc)) {
+                            return $week;
+                            break;
+                        }
+                    }
+                }
+            }
+        })->filter();
+
+        if ($data->count() == 0) {
+            return [];
+        }
+
+        $contestIds = collect($data->first())['matches'];
+        $contestIds = collect($contestIds)->flatMap(function($item){
+            return collect($item['match'])->map(function($a){
+                return $a['contestID'];
+            });
+        });
+
+        return $contestIds;
+    }
+
     public function getCurrentScheduledGames()
     {
-        $data = NFlGame::where('season',date('Y'))->get();
+        $contestIds = $this->findTheCurrentWeek();
+        $data = NFlGame::whereIn('contest_id', $contestIds)->get();
 
         $filtered =  $data->groupBy('season_type_id')->map(function($matches) {
             $matchesWithActualDate = $matches->map(function($match){
@@ -73,7 +124,7 @@ class NflApiRepository
             if ($checkDate->lte($minDate)) {
                 return $matchesWithActualDate;
             }
-        })->filter();
+        });
 
 
         if ($filtered->count() > 0) return $filtered->first();
@@ -98,6 +149,7 @@ class NflApiRepository
         $hasMatchToday = $matchesStatus->contains(function ($item) {
             return $item['todayMatch'] == true && $item['gameIsOver'] == false;
         });
+        
         
 
         return $hasMatchToday;
